0fbff7c39138024f22bd5c7b873efef360fcd4ef
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-10-20 22:46:45 +0900

    add prototype

---
+++ cpp01/ex08/Human.cpp
@@ -0,0 +1,19 @@
+#include "Human.hpp"
+
+Human::Human() {
+}
+
+Human::~Human() {
+}
+
+void Human::meleeAttack(std::string const& target) {
+}
+
+void Human::rangedAttack(std::string const& target) {
+}
+
+void Human::intimidatingShout(std::string const& target) {
+}
+
+void Human::action(std::string const& action_name, std::string const& target) {
+}
