927324e01f8521c0d26fafe466034dfd78a1706f
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-11-03 14:18:19 +0900

    add main

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -16,17 +16,19 @@ int main(void) {
   std::cout << "after post-dec: " << a << std::endl;
 
   Fixed const b(Fixed(5.05f) * Fixed(2));
-  std::cout << "b = " << b << std::endl;
+  std::cout << "b(Fixed(5.05f) * Fixed(2)) = " << b << std::endl;
   Fixed c = Fixed(10.0f) / Fixed(4);
-  std::cout << "c = " << c << std::endl;
+  std::cout << "c: Fixed(10.0f) / Fixed(4) = " << c << std::endl;
   Fixed d = Fixed(3.34f) + Fixed(4);
-  std::cout << "d = " << d << std::endl;
+  std::cout << "d: Fixed(3.34f) + Fixed(4) = " << d << std::endl;
   Fixed e;
   e = Fixed(5) - Fixed(2);
-  std::cout << e << std::endl;
+  std::cout << "e: Fixed(5) - Fixed(2) = " << e << std::endl;
 
-  std::cout << Fixed::max(a, b) << std::endl;
-  std::cout << Fixed::max(c, d) << std::endl;
+  std::cout << "Fixed::max(a, b) = " << Fixed::max(a, b) << std::endl;
+  std::cout << "Fixed::max(c, d) = " << Fixed::max(c, d) << std::endl;
+  std::cout << "Fixed::min(a, b) = " << Fixed::min(a, b) << std::endl;
+  std::cout << "Fixed::min(c, d) = " << Fixed::min(c, d) << std::endl;
 
   return 0;
 }
