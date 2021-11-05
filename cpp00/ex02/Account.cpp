dcdd00b3112f3c585f66d230d578576bf8f8f49e
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-11-05 14:23:41 +0900

    cpp00ex02 done?

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -35,7 +35,6 @@ int Account::getNbAccounts(void) {
   return _nbAccounts;
 }
 
-//  amountに変化があった時点でtotalamountを変更する
 int Account::getTotalAmount(void) {
   return _totalAmount;
 }
@@ -48,7 +47,6 @@ int Account::getNbWithdrawals(void) {
   return _totalNbWithdrawals;
 }
 
-// "[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0"
 void Account::displayAccountsInfos(void) {
   _displayTimestamp();
   std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount()
@@ -57,17 +55,42 @@ void Account::displayAccountsInfos(void) {
 }
 
 // total~へ変化量格納
+// [19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
 void Account::makeDeposit(int deposit) {
-  (void)deposit;
+  int p_amount = _amount;
+  _amount += deposit;
+  _totalAmount += deposit;
+  _nbDeposits++;
+  _totalNbDeposits++;
+  _displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";p_amount:" << p_amount
+            << ";deposit:" << deposit << ";amount:" << _amount
+            << ";nb_deposits:" << _nbDeposits << std::endl;
 }
 
+//[19920104_091532] index:0;p_amount:47;withdrawal:refused
+//[19920104_091532]
+// index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1
 bool Account::makeWithdrawal(int withdrawal) {
-  (void)withdrawal;
+  int p_amount = _amount;
+  _displayTimestamp();
+  std::cout << "index:" << _accountIndex << ";p_amount:" << p_amount
+            << ";withdrawal:";
+  if (_amount < withdrawal) {
+    std::cout << "refused" << std::endl;
+    return false;
+  }
+  _amount -= withdrawal;
+  _totalAmount -= withdrawal;
+  _nbWithdrawals++;
+  _totalNbWithdrawals++;
+  std::cout << withdrawal << ";amount:" << _amount
+            << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
   return true;
 }
 
 int Account::checkAmount(void) const {
-  return 0;
+  return _amount;
 }
 
 // [19920104_091532] index:0;amount:42;deposits:0;withdrawals:0
