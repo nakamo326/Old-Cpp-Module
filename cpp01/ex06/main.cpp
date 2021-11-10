0e0d58e0598765753ce6acd601f4ed7669e21e12
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-11-10 17:48:25 +0900

    fix main cpp01ex06

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -15,12 +15,16 @@ int main(int argc, char const *argv[]) {
   switch (level) {
     case 0:
       karen.complain("DEBUG");
+      std::cout << std::endl;
     case 1:
       karen.complain("INFO");
+      std::cout << std::endl;
     case 2:
       karen.complain("WARNING");
+      std::cout << std::endl;
     case 3:
       karen.complain("ERROR");
+      std::cout << std::endl;
       break;
     default:
       std::cout << "[ Probably complaining about insignificant problems ]"
