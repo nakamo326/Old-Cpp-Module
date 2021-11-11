da7d01a37e271d3916f7aca59b5fbbfffd094baf
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-11-11 16:03:07 +0900

    add test

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char const* argv[]) {
   if (argc != 4 || argv[1][0] == '\0' || argv[2][0] == '\0' ||
       argv[3][0] == '\0') {
-    std::cout << "USAGE: ./replace [filename] [search] [replace]" << std::endl;
+    std::cerr << "USAGE: ./replace [filename] [search] [replace]" << std::endl;
     return 1;
   }
   try {
--- cpp01/ex04/test.sh
+++ cpp01/ex04/test.sh
@@ -1,8 +1,53 @@
 #! /bin/bash
 
-function test () {
-  ./replace $1 $2 $3
-  sed=`sed -e 's/$2/$3/g' $1`
+COLOR_OK="\e[32m"
+COLOR_KO="\e[31m"
+COLOR_RESET="\e[0m"
+
+USAGE="USAGE: ./replace [filename] [search] [replace]"
+
+function sedtest () {
+  ./replace "$1" "$2" "$3"
+  sed -e "s/$2/$3/g" "$1" > sed_res
+  if diff $1.replace sed_res; then
+    printf "${COLOR_OK}OK${COLOR_RESET}\n"
+  else
+    printf "${COLOR_KO}KO${COLOR_RESET}\n"
+  fi
+  rm sed_res
+  rm $1.replace
 }
 
-test tests/test aaa zzz
+function errortest() {
+  res=`./replace "$2" "$3" "$4" 2>&1`
+  if test "$res" = "$1" ; then
+    printf "${COLOR_OK}OK${COLOR_RESET}\n"
+  else
+    printf "${COLOR_KO}KO${COLOR_RESET}\n"
+  fi
+}
+
+echo "error test"
+errortest "$USAGE" no_such_file "" ""
+errortest "$USAGE" no_such_file " " ""
+errortest "$USAGE" no_such_file "" " "
+errortest "$USAGE" "" " " " "
+errortest "$USAGE" no_such_file
+errortest "./replace: no_such_file: No such file or directory" no_such_file test test
+
+touch tests/permission
+chmod 000 tests/permission
+errortest "./replace: tests/permission: Permission denied" tests/permission test test
+chmod 644 tests/permission
+rm tests/permission
+
+touch tests/test.replace
+chmod 000 tests/test.replace
+errortest "./replace: tests/test.replace: Permission denied" tests/test test test
+chmod 644 tests/test.replace
+rm tests/test.replace
+
+echo "test with sed"
+sedtest tests/test aaa zzz
+sedtest tests/test1 cat fox
+sedtest tests/test1 " " test
