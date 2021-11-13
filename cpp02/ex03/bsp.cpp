e2b9f46fa5fe495312561250bf6c81c42a0598a1
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-11-13 23:31:08 +0900

    fix

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -3,13 +3,9 @@
 
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
   Fixed z[3];
-  // point + 二つの点から　ベクトルを二つ求める。
-  // a1b2 -a2b1
-  // abp
+
   z[0] = Point::getCross(a, b, point);
-  // bcp
   z[1] = Point::getCross(b, c, point);
-  // cap
   z[2] = Point::getCross(c, a, point);
 
   if ((z[0] < 0 && z[1] < 0 && z[2] < 0) || (z[0] > 0 && z[1] > 0 && z[2] > 0))
