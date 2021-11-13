6048e1657034a478824d0d7b275a12812fa53d48
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-11-14 00:49:12 +0900

    add implement

--- cpp03/ex03/DiamondTrap.cpp
+++ cpp03/ex03/DiamondTrap.cpp
@@ -1,14 +1,40 @@
 #include "DiamondTrap.hpp"
 
+DiamondTrap::DiamondTrap()
+    : _hp(FragTrap::_hp),
+      _energy(ScavTrap::_energy),
+      _attackDamage(FragTrap::_attackDamage) {
+  _name = std::string("");
+  std::cout << "[DiamondTrap] Default constructor called." << std::endl;
+}
+
 DiamondTrap::DiamondTrap(std::string name)
     : ClapTrap(name + "_clap_name"),
       _name(name),
       _hp(FragTrap::_hp),
       _energy(ScavTrap::_energy),
       _attackDamage(FragTrap::_attackDamage) {
+  std::cout << "[DiamondTrap] Constructor with name called." << std::endl;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap &other) {
+  std::cout << "[DiamondTrap] Copy constructor called" << std::endl;
+  *this = other;
 }
 
 DiamondTrap::~DiamondTrap() {
+  std::cout << "[DiamondTrap] Destructor called." << std::endl;
+}
+
+DiamondTrap &DiamondTrap::operator=(const DiamondTrap &rhs) {
+  std::cout << "[DiamondTrap] Assignation operator called" << std::endl;
+  if (this == &rhs)
+    return *this;
+  _name = rhs._name;
+  _hp = rhs._hp;
+  _energy = rhs._energy;
+  _attackDamage = rhs._attackDamage;
+  return *this;
 }
 
 void DiamondTrap::whoAmI() {
