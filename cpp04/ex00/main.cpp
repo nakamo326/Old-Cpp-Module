04e0ed41a7e3a4dfe9b337100d3af12a46faf195
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-11-14 23:40:15 +0900

    fix main

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -7,22 +7,30 @@
 #include "WrongCat.hpp"
 
 int main() {
-  const Animal* base = new Animal();
-  const Animal* dog = new Dog();
-  const Animal* cat = new Cat();
-  std::cout << base->getType() << std::endl;
-  std::cout << dog->getType() << std::endl;
-  std::cout << cat->getType() << std::endl;
-  base->makeSound();
-  dog->makeSound();
-  cat->makeSound();
-
-  std::cout << "===WrongAnimal===" << std::endl;
-  const WrongAnimal* wbase = new WrongAnimal();
-  const WrongAnimal* wcat = new WrongCat();
-  std::cout << wbase->getType() << std::endl;
-  std::cout << wcat->getType() << std::endl;
-  wbase->makeSound();
-  wcat->makeSound();
+  {
+    const Animal* base = new Animal();
+    const Animal* dog = new Dog();
+    const Animal* cat = new Cat();
+    std::cout << base->getType() << std::endl;
+    std::cout << dog->getType() << std::endl;
+    std::cout << cat->getType() << std::endl;
+    base->makeSound();
+    dog->makeSound();
+    cat->makeSound();
+    delete base;
+    delete dog;
+    delete cat;
+  }
+  {
+    std::cout << "===WrongAnimal===" << std::endl;
+    const WrongAnimal* wbase = new WrongAnimal();
+    const WrongAnimal* wcat = new WrongCat();
+    std::cout << wbase->getType() << std::endl;
+    std::cout << wcat->getType() << std::endl;
+    wbase->makeSound();
+    wcat->makeSound();
+    delete wbase;
+    delete wcat;
+  }
   return 0;
 }
