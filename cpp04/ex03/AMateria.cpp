b9fceadea21144a5bc31212f77a26786b3720f20
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-11-15 23:06:34 +0900

    implement amateria

--- cpp04/ex03/AMateria.cpp
+++ cpp04/ex03/AMateria.cpp
@@ -1,27 +1,20 @@
 #include "AMateria.hpp"
 
-AMateria::AMateria() {
-}
+AMateria::AMateria() : _type("none") {}
 
-AMateria::AMateria(std::string const &type) {
-}
+AMateria::AMateria(std::string const &type) : _type(type) {}
 
-AMateria::AMateria(const AMateria &other) {
-  *this = other;
-}
+AMateria::AMateria(const AMateria &other) { *this = other; }
 
-AMateria::~AMateria() {
-}
+AMateria::~AMateria() {}
 
 AMateria &AMateria::operator=(const AMateria &rhs) {
   if (this == &rhs)
     return *this;
-
+  _type = rhs._type;
   return *this;
 }
 
-std::string const &AMateria::getType() const {
-}
+std::string const &AMateria::getType() const { return _type; }
 
-void AMateria::use(ICharacter &target) {
-}
+void AMateria::use(ICharacter &target) {}
