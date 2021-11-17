2e2dc02495cc0b847e24db02b54f9447c6a130f2
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-11-17 13:24:48 +0900

    wip

---
+++ cpp05/ex01/Form.cpp
@@ -0,0 +1,48 @@
+#include "Form.hpp"
+
+Form::Form()
+    : _isSigned(false),
+      _name("nameless"),
+      _gradeToSign(1),
+      _gradeToExecute(1) {}
+
+Form::Form(const std::string &name, unsigned int gradeToSign,
+           unsigned int gradeToExecute)
+    : _isSigned(false),
+      _name(name),
+      _gradeToSign(gradeToSign),
+      _gradeToExecute(gradeToExecute) {
+  if (_gradeToSign < 1 || _gradeToExecute < 1)
+    throw GradeTooHighException();
+  if (_gradeToSign > 150 || _gradeToExecute > 150)
+    throw GradeTooLowException();
+}
+
+Form::Form(const Form &other)
+    : _isSigned(other._isSigned),
+      _name(other._name),
+      _gradeToSign(other._gradeToSign),
+      _gradeToExecute(other._gradeToExecute) {}
+
+Form::~Form() {}
+
+Form &Form::operator=(const Form &rhs) {
+  if (this == &rhs)
+    return *this;
+  const_cast<std::string &>(_name) = rhs._name;
+  const_cast<unsigned int &>(_gradeToSign) = rhs._gradeToSign;
+  const_cast<unsigned int &>(_gradeToExecute) = rhs._gradeToExecute;
+  return *this;
+}
+
+void Form::beSigned(const Bureaucrat &b) {}
+
+const char *Form::GradeTooHighException::what() const throw() {
+  return "Bureaucrat's grade will be too high!";
+}
+
+const char *Form::GradeTooLowException::what() const throw() {
+  return "Bureaucrat's grade will be too low!";
+}
+
+std::ostream &operator<<(std::ostream &stream, const Form &f) {}
