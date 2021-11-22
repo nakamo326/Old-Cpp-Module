6eebd125f862f8113dbf229f0392a4bdfcce9fdd
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-11-22 16:36:44 +0900

    fix

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -11,14 +11,10 @@ private:
 
 public:
   Array();
-  Array(size_t n) {
-    _data = new Elm[n]();
-    _size = n;
-  }
+  Array(size_t n) : _data(new Elm[n]()), _size(n) {}
 
-  Array(const Array &other) {
-    _data = new Elm[other._size]();
-    _size = other._size;
+  Array(const Array &other)
+      : _data(new Elm[other._size]()), _size(other._size) {
     for (size_t i = 0; i < _size; i++) {
       _data[i] = other._data[i];
     }
@@ -40,25 +36,19 @@ public:
 
   const Elm &operator[](size_t index) const {
     if (index >= _size || index < 0) {
-      throw ArrayIndexOutOfRange();
+      throw std::out_of_range("Access to out of range!");
     }
     return _data[index];
   }
 
   Elm &operator[](size_t index) {
     if (index >= _size || index < 0) {
-      throw ArrayIndexOutOfRange();
+      throw std::out_of_range("Access to out of range!");
     }
     return _data[index];
   }
 
   size_t size() const { return _size; }
-
-  class ArrayIndexOutOfRange : public std::out_of_range {
-  public:
-    ArrayIndexOutOfRange(const std::string &msg = "Access to out of range!")
-        : out_of_range(msg){};
-  };
 };
 
 #endif  // ARRAY_HPP
