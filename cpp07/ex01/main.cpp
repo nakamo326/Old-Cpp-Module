e6123f05d875c98c1e5f062c5705d6d6a08aa2bb
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-11-22 11:21:52 +0900

    fix main

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -7,7 +7,7 @@ int main(void) {
   ::iter(arr0, (size_t)(sizeof(arr0) / sizeof(arr0[0])), ::times);
   ::iter(arr0, (size_t)(sizeof(arr0) / sizeof(arr0[0])), ::print);
   std::cout << std::endl;
-
+  std::cout << std::endl;
   double arr1[5] = {1.5, 2.8, 7.6, 9.9, 13.2};
   ::iter(arr1, (size_t)(sizeof(arr1) / sizeof(arr1[0])), ::print);
   std::cout << std::endl;
